5d4b7ba9758ca8efaf9952d8baa279edce9207cf
nsanthip <63122660106@g.lpru.ac.th>
2024-03-14 16:49:30 +0700

    first commit

---
+++ lib/themes/colors.dart
@@ -0,0 +1,14 @@
+//fimp
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFF000000);   //0x -> # , FF -> light 100%
+const primaryDark = Color(0xFFB50DF2);
+const primaryLight = Color(0xFFB2DFDB);
+const primaryText = Color(0xFF212121);
+const secondaryText = Color(0xFF757575);
+const accent = Color(0xFF757575);
+const icons = Color(0xFFFFFFFF);
+const divider = Color(0xFFBDBDBD);
+
+
+
